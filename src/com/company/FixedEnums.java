cd080682f68a256f43ea774a50c59e330c0b9a34
Gimpei <angeldust03@gmail.com>
2014-10-18 20:11:30 +0900

    add method sample

--- src/com/company/FixedEnums.java
+++ src/com/company/FixedEnums.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Set;
+
 /**
  * Created by Gimpei on 2014/10/18.
  */
@@ -18,4 +20,26 @@ enum Operation{
     }
     @Override public String toString(){ return symbol; }
     abstract String apply();
+}
+
+class Text{
+    public enum Style{BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+    public void applyStyles(Set<Style> styles){
+    }
+}
+
+/**
+ * 順序インデックスの代わりにEnumMapを使用する
+ */
+class Herb{
+    public enum Type{ ANNUAL, PERENNIAL, BIENNIAL}
+    private final String name;
+    private final Type type;
+    Herb(String name, Type type){
+        this.name = name;
+        this.type = type;
+    }
+    @Override public String toString(){
+        return name;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Type;
-import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.*;
 
 interface MapInterface<T, K>{
     //public static Type type = T.class;
@@ -21,7 +26,23 @@ class AMap implements MapInterface<String, String> {
     }
 }
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Test{
+}
+
 public class Main {
+    /**
+     * 関数の説明をがっちり書く
+     * @param m 正でなければならない
+     * @return　mのモジュラス値
+     * @throws java.lang.ArithmeticException m <= 0の場合
+     */
+    public BigInteger mod(BigInteger m){
+        if( m.signum() <= 0)
+            throw new ArithmeticException("Modulus <= 0:" + m );
+        return BigInteger.TEN;
+    }
     public static void main(String[] args) {
 	  String s = "something";
       System.out.println(s);
@@ -31,5 +52,15 @@ public class Main {
 
       for( Operation o : Operation.values() )
           System.out.println(o.toString());
+
+      //BitFieldの代わりにEnumSetを利用する
+      Text text = new Text();
+      text.applyStyles(EnumSet.of(Text.Style.BOLD, Text.Style.ITALIC));
+
+      //順序インデックスの代わりにEnumMapを利用する
+      Map<Herb.Type, Set<Herb>> herbsByType = new EnumMap<Herb.Type, Set<Herb>>(Herb.Type.class);
+      for(Herb.Type t : Herb.Type.values())
+          herbsByType.put(t, new HashSet<Herb>());
+      System.out.println(herbsByType);
     }
 }
